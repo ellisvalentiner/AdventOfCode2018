ff6b84cfa44dc807cdb87bed6a17e2ad2297bd67
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-12-01 21:15:15 +0000

    part two is wrong

--- src/problem1.jl
+++ src/problem1.jl
@@ -1,19 +1,25 @@
 
 function problem1(datadir::String=joinpath(@__DIR__, "data"))
-    instructions = readlines(joinpath(datadir, "1.txt"))
+    instructions = readlines(normpath(joinpath(datadir, "1.txt")))
     changes = Meta.parse.(instructions)
-    partone = last(cumsum(changes))
+    x = cumsum(changes)
+    partone = last(x)
     frequencies = [0]
-    i = 1
-    n = length(changes)
-    while notfound
-        frequency = last(frequencies) + changes[i % n]
-        if frequency in frequencies
-            parttwo = frequency
+    while length(frequencies) == length(unique(frequencies))
+        y = last(frequencies) .+ x
+        append!(frequencies, y)
+    end
+    for i in 1:length(frequencies)
+        val = frequencies[i]
+        n = count(x->x == val, frequencies)
+        @info "$val\t$n"
+        if n == 2
+            global parttwo = val
             break
         end
-        append!(frequencies, [frequency])
-        i += 1
     end
     return Dict("Part One" => partone, "Part Two" => parttwo)
 end
+
+@info problem1()
+

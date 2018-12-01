389e7205b3e9e347b144d707edd065bb4318b265
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-12-01 17:23:34 +0000

    slow solve parttwo

--- src/problem1.jl
+++ src/problem1.jl
@@ -1,4 +1,19 @@
 
 function problem1(datadir::String=joinpath(@__DIR__, "data"))
-    
+    instructions = readlines(joinpath(datadir, "1.txt"))
+    changes = Meta.parse.(instructions)
+    partone = last(cumsum(changes))
+    frequencies = [0]
+    i = 1
+    n = length(changes)
+    while notfound
+        frequency = last(frequencies) + changes[i % n]
+        if frequency in frequencies
+            parttwo = frequency
+            break
+        end
+        append!(frequencies, [frequency])
+        i += 1
+    end
+    return Dict("Part One" => partone, "Part Two" => parttwo)
 end

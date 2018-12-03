50dcc6915ccd7b4af1c053ac035eb8aef2f8742a
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-12-03 13:22:29 -0500

    Add day 3

--- src/AdventOfCode2018.jl
+++ src/AdventOfCode2018.jl
@@ -2,5 +2,6 @@ module AdventOfCode2018
 
 include("puzzle1.jl")
 include("puzzle2.jl")
+include("puzzle3.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,4 +5,5 @@ using Test
 @testset begin
     @test puzzle1() == Dict("Part One"=>538,"Part Two"=>77271)
     @test puzzle2() == Dict("Part One"=>6448,"Part Two"=>"evsialkqyiurohzpwucngttmf")
+    @test puzzle3() == Dict("Part One"=>121163,"Part Two"=>943)
 end

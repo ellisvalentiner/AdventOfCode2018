7d81fbcbab0f1d1cd720c0d47c6b750176a8e623
Ellis Valentiner <ellisvalentiner@gmail.com>
2018-12-03 19:22:01 -0500

    Cleanjp 3

--- README.jl
+++ README.jl
@@ -8,27 +8,32 @@
 
 #+ echo=false
 using AdventOfCode2018
+using BenchmarkTools
 using Markdown
 import Markdown: MD, Table
-import Statistics: median
+import Statistics: median, mean
 
-table = ["Day" "Elapsed (s)" "Allocated (MiB)" "Garbage Collection (s)"]
+table = ["Day" "Memory (MiB)" "Allocations" "Min. Time (ms)" "Median Time (ms)" "Mean Time (ms)" "Max. Time (ms)" "Samples"]
 for puzzle in filter(x-> x≠ :AdventOfCode2018, names(AdventOfCode2018))
     m = match(r"[0-9]+", String(puzzle))
     day = string(parse(Int, m.match))
     @info "Puzzle $day"
     @eval begin
-        val, t, bytes, gctime, memallocs = @timed $puzzle()
-        t = round(t, digits=4)
-        kib = round(bytes/1024^2, digits=4)
-        gctime = round(gctime, digits=4)
-        row = [$day t kib gctime]
+        result = @benchmark $puzzle()
+        memory = round(result.memory / 1024^2, digits=2)
+        allocations = result.allocs
+        mintime = round(minimum(result.times) / 1_000_000, digits=3)
+        mediantime = round(median(result.times) / 1_000_000, digits=3)
+        meantime = round(mean(result.times) / 1_000_000, digits=3)
+        maxtime = round(minimum(result.times) / 1_000_000, digits=3)
+        n = length(result.times)
+        row = [$day memory allocations mintime mediantime meantime maxtime n]
         table = vcat(table, row)
     end
 end
 table = vcat(table, ["**Median**" round.(median(table[2:end, 2:end], dims=1), digits=4)])
 table = vcat(table, ["**Total**" round.(sum(table[2:(end-1), 2:end], dims=1), digits=4)])
-performance = MD(Table(Any[map(x->Any[x], table[i, :]) for i in 1:size(table, 1)], Symbol[:l, :r, :r, :r]))
+performance = MD(Table(Any[map(x->Any[x], table[i, :]) for i in 1:size(table, 1)], Symbol[:l, :r, :r, :r, :r, :r, :r, :r]))
 
 #' ## Performance
 
--- src/puzzle3.jl
+++ src/puzzle3.jl
@@ -1,11 +1,9 @@
 
 function parse_input(line::String)
-    id = match(r"\#\d", line).match
     offset = match(r"\d+,\d+", line).match
     left, top = parse.(Int, split(string(offset), ','))
     dimensions = match(r"\d+x\d+", line).match
     width, height = parse.(Int, split(string(dimensions), 'x'))
-    area = width * height
     squares = [(left + i, top + j) for i=1:width, j=1:height]
     return reshape(squares, (length(squares)))
 end
@@ -13,12 +11,13 @@ end
 function puzzle3(datadir::String=joinpath(@__DIR__, "data"))
     instructions = readlines(normpath(joinpath(datadir, "3.txt")))
     claims = parse_input.(instructions)
-    squarecounts = countmap(reduce(vcat, claims))
-    partOne = sum(values(squarecounts) .> 1)
+    squareCounts = countmap(reduce(vcat, claims))
+    partOne = count(values(squareCounts) .> 1)
     partTwo = 0
 
     for i in 1:length(claims)
-        if all([squarecounts[Tuple(k)] for k in claims[i]] .== 1)
+        claim = claims[i]
+        if all([squareCounts[k] for k in claim] .== 1)
             partTwo = i
             break
         end
